68cecc28053c59593662c8af3d2b7143b6f9658b
MOISE PIERRE-LOUIS <106705081+koatz55@users.noreply.github.com>
2023-03-12 09:58:08 -0400

    '2 commit from vs'

---
+++ week1/predictDestructuring.js
@@ -0,0 +1,56 @@
+// Problem 1: Syntax Error line 8 / I was wrong Tesla Mercedes
+// Problem 2: Elon , employee / Error on console.log(name)
+// Problem 3: Syntax Error message type is not the same as original constant./ 12345 undefined
+// Problem 4: False ,True / correct
+// Problem 5: Runtime Error message line 4 /value , (6) [1, 5, 1, 8, 3, 3] , 1, 5
+
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+console.log(otherRandomCar)
+
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+// console.log(name)
+console.log(employee.name);
+console.log(otherName);
+
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password);
+console.log(hashedPassword);
+
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second);
+console.log(first == third);
+
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+
