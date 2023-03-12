b4c1db03d05665d836b90a8c980172ec1477d40a
MOISE PIERRE-LOUIS <106705081+koatz55@users.noreply.github.com>
2023-03-12 19:12:28 -0400

    1s random commit

---
+++ week1/randomCode.js
@@ -0,0 +1,35 @@
+const animals = ['horse', 'dog', 'fish', 'cat', 'bird'];
+// const [firstAnimal, secondAnimal] = animals;
+
+// console.log(firstAnimal, secondAnimal);
+// => ['horse', 'dog']
+const [firstAnimal, secondAnimal, ...otherAnimals] = animals;
+console.log(otherAnimals);
+// => ['fish', 'cat', 'bird']
+
+const person = {
+    firstName: 'Bob',
+    lastName: 'Marley',
+    email: 'bob@marley.com',
+    password: 'sekureP@ssw0rd9',
+    username: 'barley',
+    addresses: [
+        {
+            address: '1600 Pennsylvania Avenue',
+            city: 'Washington, D.C.',
+            zipcode: '20500',
+        },
+        {
+            address: '221B Baker St.',
+            city: 'London',
+            zipcode: 'WC2N 5DU',
+        }
+    ],
+    createdAt: 1543945177623
+};
+
+
+
+const { firstName, lastName, ...attributes } = person;
+
+
