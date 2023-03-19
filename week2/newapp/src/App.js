1dc93fa9472e3317fd6ea8a758e076c388c1a7f0
MOISE PIERRE-LOUIS <106705081+koatz55@users.noreply.github.com>
2023-03-19 13:12:13 -0400

    first git app

---
+++ week2/newapp/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className ="Greeting">Hello Dojo!</h1>
+        <h2>Things I need to do:</h2>
+        <p>
+          * Learn React 
+          * Turn in at least 60% core assignments
+          * DQs
+          * Feed my cat and dog 
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
