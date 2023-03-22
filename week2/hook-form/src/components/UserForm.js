c2d0cedde8605bcee6ae151959ab78f690eb39bf
MOISE PIERRE-LOUIS <106705081+koatz55@users.noreply.github.com>
2023-03-22 16:46:41 -0400

    updated version

--- week2/hook-form/src/components/UserForm.js
+++ week2/hook-form/src/components/UserForm.js
@@ -6,19 +6,55 @@ const UserForm = (props) => {
     const [password, setPassword] = useState("");
     const [confirmPassword , setConfirmPassword] = useState("");  
     const [usernameError, setUsernameError] = useState("");
-    
+    const [emailError, setEmailError] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+    const [confirmPasswordError, setConfirmPasswordError] = useState(""); 
+
     const handleUsername = (e) => {
         setUsername(e.target.value);
         if(e.target.value.length < 1) {
             setUsernameError("Username is required!");
-        } else if(e.target.value.length < 3) {
-            setUsernameError("Username must be 3 characters or longer!");
+        } else if(e.target.value.length < 2) {
+            setUsernameError("Username must be 2 characters or longer!");
         } else {
             // an empty string is considered a "falsy" value
             setUsernameError("");
         };
     };
-    
+
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+        if (e.target.value.length < 1) {
+            setEmailError("Email is required!");
+        } else if(e.target.value.length < 5) {
+            setEmailError("Email must be at least 5 characters!");
+        } else {
+            setEmailError("");
+        };
+    }
+
+    const handlePassword = (e) => {
+        setPassword(e.target.value);
+        if (e.target.value.length < 8) {
+            setPasswordError("Password must be at least 8 characters!");
+        } else if (e.target.value !== confirmPassword) {
+            setPasswordError("Passwords do not match");
+        } else {
+            setPasswordError("")
+        };
+    }
+
+    const handleConfirmPassword = (e) => {
+        setConfirmPassword(e.target.value);
+        if (e.target.value.length < 8) {
+            setConfirmPasswordError("Password must be at least 8 characters!");
+        } else if (e.target.value !== password) {
+            setConfirmPasswordError("Passwords do not match");
+        } else {
+            setConfirmPasswordError("")
+        };
+    }
+
     const createUser = (e) => {
         // we must prevent the default refresh of the browser to keep our state from being reset
         e.preventDefault();
@@ -46,27 +82,42 @@ const UserForm = (props) => {
         <form onSubmit={ createUser }>
             <div>
                 <label>Username: </label> 
-                <input type="text" onChange={ handleUsername } />
+                <input type="text" value={username} onChange={ handleUsername } />
                     {
                         usernameError ?
                         <p>{ usernameError }</p> :
                         ''
-                    }type="text" value={username} onChange={ (e) => setUsername(e.target.value) } />
+                    } 
             </div>
             <div>
                 <label>Email Address: </label> 
-                <input type="text" value={email} onChange={ (e) => setEmail(e.target.value) } />
+                <input type="text" value={email} onChange={ handleEmail } />
+                    {
+                        emailError ?
+                        <p>{ emailError }</p> :
+                        ''
+                    }
             </div>
             <div>
                 <label>Password: </label>
-                <input type="text" value={password} onChange={ (e) => setPassword(e.target.value) } />
+                <input type="text" value={password} onChange={ handlePassword } />
+                    {
+                        passwordError ?
+                        <p>{ passwordError }</p> :
+                        ''
+                    }
             </div>
             <div>
                 <label>confirmPassword: </label>
-                <input type="text" value={confirmPassword} onChange={ (e) => setConfirmPassword(e.target.value) } />
+                <input type="text" value={confirmPassword} onChange={ handleConfirmPassword } />
+                {
+                    confirmPasswordError ?
+                    <p>{ confirmPasswordError }</p> :
+                    ''
+                }
             </div>
             {
-                usernameError ?
+                usernameError || emailError || passwordError || confirmPasswordError ?
                 <input type="submit" value="Create User" disabled /> : 
                 <input type="submit" value="Create User" />
             }
