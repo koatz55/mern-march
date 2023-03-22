279f8f498bec0532ad64b8bb1cb02adc7f1fdda6
MOISE PIERRE-LOUIS <106705081+koatz55@users.noreply.github.com>
2023-03-21 22:41:26 -0400

    user form

--- week2/hook-form/src/components/UserForm.js
+++ week2/hook-form/src/components/UserForm.js
@@ -5,6 +5,19 @@ const UserForm = (props) => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [confirmPassword , setConfirmPassword] = useState("");  
+    const [usernameError, setUsernameError] = useState("");
+    
+    const handleUsername = (e) => {
+        setUsername(e.target.value);
+        if(e.target.value.length < 1) {
+            setUsernameError("Username is required!");
+        } else if(e.target.value.length < 3) {
+            setUsernameError("Username must be 3 characters or longer!");
+        } else {
+            // an empty string is considered a "falsy" value
+            setUsernameError("");
+        };
+    };
     
     const createUser = (e) => {
         // we must prevent the default refresh of the browser to keep our state from being reset
@@ -13,9 +26,9 @@ const UserForm = (props) => {
         // shorthand ES6 syntax for building an object - see notes above
         const newUser = { username, email, password, confirmPassword};
         console.log("Welcome", newUser);
-    	setUsername("");
-    	setEmail("");
-    	setPassword("");
+        setUsername("");
+        setEmail("");
+        setPassword("");
         setConfirmPassword("");
         return (
             <div className='newUser'>
@@ -26,13 +39,19 @@ const UserForm = (props) => {
             </div>
         );
     };
+
     
     return(
     <div>
         <form onSubmit={ createUser }>
             <div>
                 <label>Username: </label> 
-                <input type="text" value={username} onChange={ (e) => setUsername(e.target.value) } />
+                <input type="text" onChange={ handleUsername } />
+                    {
+                        usernameError ?
+                        <p>{ usernameError }</p> :
+                        ''
+                    }type="text" value={username} onChange={ (e) => setUsername(e.target.value) } />
             </div>
             <div>
                 <label>Email Address: </label> 
@@ -46,7 +65,11 @@ const UserForm = (props) => {
                 <label>confirmPassword: </label>
                 <input type="text" value={confirmPassword} onChange={ (e) => setConfirmPassword(e.target.value) } />
             </div>
-            <input type="submit" value="Create User" />
+            {
+                usernameError ?
+                <input type="submit" value="Create User" disabled /> : 
+                <input type="submit" value="Create User" />
+            }
         </form>
         <div className='newUser'>
             <p>Username: {username}</p>
