c5b9a73bdb000c0799cb359cd301d69c2f5c6c1d
MOISE PIERRE-LOUIS <106705081+koatz55@users.noreply.github.com>
2023-03-26 17:18:55 -0400

    updated validation

--- week2/super-form/src/components/Box.jsx
+++ week2/super-form/src/components/Box.jsx
@@ -4,15 +4,9 @@ const Box = (props) => {
     // const [color, setColor] = useState("");
     const {boxList, setBoxList} = props
     const [box, setBox] = useState({ color:""})
-    const [colorError ,setColorError] = useState("");
     
     const handleColor = (e) => {
         setBox({...box, [e.target.name]: e.target.value});
-        // if (e.target.value < 1 ){
-        //     setColorError("color must not be empty");
-        // }else {
-        //     setColorError("");
-        // }
     };
 
     const createBox = (e) => {
@@ -29,18 +23,18 @@ const Box = (props) => {
         <div>
         <form onSubmit={ createBox }>
             <div>
+                {
+                    box.color.length < 1 ?
+                    <p>Enter color name or a hexadecimal value </p>:
+                    null
+                }
                 <label>Color: </label> 
                 <input type="text" name="color" value={box.color} onChange={ handleColor } />
-                    {
-                        colorError ?
-                        <p>{ colorError }</p> :
-                        ''
-                    } 
-            {
-                colorError ?
-                <input type="submit" value="ADD" disabled /> : 
-                <input type="submit" value="ADD" />
-            }
+                {
+                    box.color < 1  ?
+                    <input type="submit" value="ADD" disabled /> : 
+                    <input type="submit" value="ADD" />
+                }
             </div>
         </form>
     </div>
