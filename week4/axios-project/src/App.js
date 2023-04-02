4a06f14cdd2ab694cd8836245fcc2ab16dde469d
MOISE PIERRE-LOUIS <106705081+koatz55@users.noreply.github.com>
2023-04-01 22:05:32 -0400

    pokemon api with axios!

--- week4/axios-project/src/App.js
+++ week4/axios-project/src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 
 function App() {
+  const[pokemon, setPokemon] = useState([]);
+  
+  useEffect(() => {
+    axios.get('http://pokeapi.co/api/v2/pokemon')
+      .then((response) =>{
+        console.log('RESPONSE: ' + JSON.stringify(response));
+        setPokemon(response.data.results);
+      })
+      .catch((err) =>{
+        console.log(err);
+      })
+  },[]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>pokemon api with axios!</h1>
+      {
+        pokemon.map((mon, idx) => (
+          <div key= {idx}>
+            <h2 className='Pokemon'><span className='Tag'>Kanto {idx +1}</span> {mon.name}</h2>
+          </div>
+        ))
+      }
     </div>
   );
 }
