8fdadb61027ac34ff7d375f716672d6b82522450
MOISE PIERRE-LOUIS <106705081+koatz55@users.noreply.github.com>
2023-04-04 18:13:51 -0400

    just started

---
+++ week4/routing-practice/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import { 
+  BrowserRouter, 
+  Routes,
+  Route,
+  Link,
+} from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+
+        </header>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
