2717a9127b0b4678f6bb2f98c43e2ba42a310316
MOISE PIERRE-LOUIS <106705081+koatz55@users.noreply.github.com>
2023-04-06 00:28:16 -0400

    will finnish later

---
+++ week4/routing-practice/src/Components/Numbers.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useParams } from "react-router-dom"; 
+
+const Numbers = (props) => { 
+    const { num } = useParams();
+    
+    return (
+    <div className="Home">
+        <h1 >Number is { num }!</h1>
+    </div>
+    );
+}
+
+export default Numbers;
