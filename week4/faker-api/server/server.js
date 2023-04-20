e2581362efafa4025c91684c41285b11064603b4
MOISE PIERRE-LOUIS <106705081+koatz55@users.noreply.github.com>
2023-04-19 21:05:24 -0400

    started to use mongod compass

---
+++ week4/faker-api/server/server.js
@@ -0,0 +1,71 @@
+
+const express = require("express");
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/name_of_your_DB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log('Established a connection to the database'))
+    .catch(err => console.log('Something went wrong when connecting to the database ', err));
+
+
+const app = express();
+const port = 8000;
+const { faker } = require('@faker-js/faker');
+
+// ! make sure these lines are above any app.get and app.post code blocks
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+console.log(faker.name.firstName());
+
+
+const createUser = () => {
+    return {
+        _id: faker.datatype.uuid(),
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        phoneNumber: faker.phone.number(),
+        email: faker.internet.email(),
+        password: faker.internet.password(8, true)
+    }
+}
+
+const createCompany = () => {
+    return {
+        _id: faker.datatype.uuid(),
+        name: faker.company.name(),
+        address: {
+            street: faker.address.streetAddress(true),
+            country: faker.address.country(), 
+            city: faker.address.city(),
+            state: faker.address.state(), 
+            zip: faker.address.zipCodeByState()
+        }
+    }
+}
+app.post('api/users/new', (req, res) => {
+    const newUser = createUser();
+    users.push(req.body);
+    console.log(req.body);
+    res.json(users)
+})
+
+app.get('api/companies/new', (req, res) => {
+    const newCompany = createCompany();
+    res.json(newCompany)
+})
+
+const users = []
+
+let user1 = createUser();
+console.log(user1)
+
+let company1 = createCompany();
+console.log(company1)
+
+
+
+
+app.listen( port, () => console.log(`Listening on port: ${port} woohoo!`) );
