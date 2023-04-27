bdb5f829ff0a8c33a94ab0e11f87a16cbec427bc
MOISE PIERRE-LOUIS <106705081+koatz55@users.noreply.github.com>
2023-04-27 00:02:25 -0400

    2

--- week6/moiseProjectManager/client/src/App.js
+++ week6/moiseProjectManager/client/src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
-import ProductForm from './components/ProductForm';
+// import ProductForm from './components/ProductForm';
+import { useState } from 'react';
+import axios from 'axios';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Main from '../views/Main';
+import Detail from '../views/Detail';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ProductForm/>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<Main/>} path="/home" default />
+            <Route element={<Detail/>} path="/people/:id" /> 
+          </Routes>
+        </BrowserRouter>
       </header>
     </div>
   );
